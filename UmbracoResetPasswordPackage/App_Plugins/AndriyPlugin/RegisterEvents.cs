b00a991417759b6aab0190171380fc33b02e412f
Andriy <andriy@lakhno.com>
2015-04-23 20:31:51 +0300

    Initi Checkin

---
+++ UmbracoResetPasswordPackage/App_Plugins/AndriyPlugin/RegisterEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core;
+
+namespace UmbracoResetPasswordPackage.App_Plugins.AndriyPlugin
+{
+  public class RegisterEvents : ApplicationEventHandler
+  {
+    protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+    {
+       RouteTable.Routes.MapRoute(
+            "AndriyCustomRoute",
+            "ResetUserPassword/{action}/{id}",
+            new
+                {
+                    controller = "ResetUserPassword", 
+                    action = "Index", 
+                    id = UrlParameter.Optional
+                });           
+      base.ApplicationStarted(umbracoApplication, applicationContext);
+    }
+  }
+}
---
+++ UmbracoResetPasswordPackage/App_Plugins/AndriyPlugin/ResetUserPasswordController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoResetPasswordPackage.App_Plugins.AndriyPlugin
+{
+
+  public class ResetUserPasswordController : PluginController
+  {
+    public ResetUserPasswordController()
+      : this(UmbracoContext.Current)
+    {
+    }
+
+    public ResetUserPasswordController(UmbracoContext umbracoContext)
+      : base(umbracoContext)
+    {
+    }
+    public ActionResult Index()
+    {
+
+      string password = "Andriy123";
+      string Hash = Umbraco.CreateMd5Hash(password);
+      ViewBag.password=password;
+      ViewBag.Hash=Hash;
+      ViewBag.NewHash=Umbraco.CreateMd5Hash(password);
+      return View();
+    }
+  }
+}
